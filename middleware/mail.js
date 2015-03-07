c12498e8105b3ca93ffc81fbae8fd1d9434b5a28
Yashprit Singh <yashprits@gmail.com>
2015-03-07 16:40:28 +0530

    renamed bluebird

--- middleware/mail.js
+++ middleware/mail.js
@@ -2,7 +2,7 @@ var nodemailer = require('nodemailer'),
   path = require('path'),
   templatesDir = path.join(__dirname, '../public/templates'),
   emailTemplates = require('email-templates'),
-  Promise = require("BlueBird"),
+  Promise = require("bluebird"),
   config = require("../config/config");
 
 module.exports = function(to, text) {
